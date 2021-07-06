f9338dc8282d0d30870f16f85fdc645894ee4907
Josh <joshuamercier@yahoo.com>
2021-07-06 18:16:27 -0400

    Player control of character and camera implemented fully.

--- Assets/Course Library/Scripts/RotateCamera.cs
+++ Assets/Course Library/Scripts/RotateCamera.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    // Class variables
+    public float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.down, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
